d4e39ac18a34846d1c6253f98aef1610b3903ae8
Jim Verwimp <jim.verwimp@uliege.be>
2019-08-22 15:50:36 +0200

    JPA Repository on MySQL database exposed by a Spring Boot Application

--- spring-boot-user-manager/spring-boot-user-manager-webservice/src/main/java/be/jimve/springbootusermanager/ws/Application.java
+++ spring-boot-user-manager/spring-boot-user-manager-webservice/src/main/java/be/jimve/springbootusermanager/ws/Application.java
@@ -10,10 +10,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = "be.jimve.springbootusermanager.repositories")
-// For the wiring to be done, we need to enable JPA Repositories
 @ComponentScan(basePackages = {"be.jimve.springbootusermanager.services", "be.jimve.springbootusermanager.ws"})
-// We need to scan the Service package as well as the controller's.
-@EntityScan(basePackages = "be.jimve.springbootusermanager.beans") // Our entities will be defined in the beans package
+@EntityScan(basePackages = "be.jimve.springbootusermanager.beans") // Our entities will be defined in the beans package*/
 public class Application extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
